4055de9401a990f421e65bdb339ec355e292afc9
Shaf-Shafiq12 <95437523+Shaf-Shafiq12@users.noreply.github.com>
2021-12-03 01:43:54 +0500

    Update First_File.java

--- First_File.java
+++ First_File.java
@@ -1,11 +1,7 @@
 
 public class First_File {
 	public static void main (String [] args) {
-		<<<<<<< Shaf_Shafiq_i201864
-		String name = "Shaf from branch Shaf_Shafiq_i201864";
-		=======
-		String name = "Shaf from branch Shaf_Shafiq_i201864(2)";
-        >>>>>>> main
+		String name = "Shaf Shafiq";
 		String rollno = "20i-1864";
 		
 		System.out.println("Name: " + name);
