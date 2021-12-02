ebc4ce291321b28120abba3ce25ad2865bb1250c
Shaf-Shafiq12 <95437523+Shaf-Shafiq12@users.noreply.github.com>
2021-12-03 01:00:45 +0500

    Create First_File.java

---
+++ First_File.java
@@ -0,0 +1,10 @@
+
+public class First_File {
+	public static void main (String [] args) {
+		String name = "Shaf Shafiq";
+		String rollno = "20i-1864";
+
+		System.out.println("Name: " + name);
+		System.out.println("RollNo: " + rollno);
+	}
+}
